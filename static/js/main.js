f16febc4b62af67afc986ab3c207f969fd90640a
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2024-09-09 15:43:58 -0400

    file upload works

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,31 @@
 $(document).ready(function () {
 
     $("#submit_meme_btn").click(function () {
-        console.log("Generate caption from meme...")
-        $("#spinner-div").show()
-        if ($("#image_input").val() == null) {
-            alert("there is no image!")
-        }
+        send_img_to_gpt()
     })
 
-    function send_img_to_gpt(){
-        return
+    function send_img_to_gpt() {
+        let image = $("#image_input")[0].files[0]
+        if (image) {
+            let formData = new FormData();
+            formData.append("image", image); // Append the file to FormData
+
+            $.ajax({
+                url: "/upload_image",
+                type: "POST",
+                data: formData,
+                contentType: false,
+                processData: false,
+                success: function (response) {
+                    console.log(response);
+                },
+                error: function (xhr, status, error) {
+                    console.error(xhr.responseText);
+                }
+            });
+        } else {
+            console.error("No file selected.");
+        }
     }
 
 })
